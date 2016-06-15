83e9bc06263acfa22a9dbf0dcde765ec505228eb
Dennis Fricke <github@dennis-fricke.de>
2016-06-15 16:00:55 +0200

    + added null checking for complex objects

--- Parsing/ClassParser.php
+++ Parsing/ClassParser.php
@@ -85,31 +85,33 @@ class ClassParser
 	private function buildArray($obj)
 	{
 		$result = array();
-		if (is_array($obj) || $obj instanceof Countable) {
-			foreach ($obj as $key => $value) {
-				$result[$key] = $this->buildArray($value);
-			}
-		} else if (is_string($obj) || is_bool($obj) || is_integer($obj) || is_float($obj) || is_double($obj)) {
-			$result = $obj;
-		} else if (is_array($obj)) {
-			$result[] = $this->buildArray($obj);
-		} else {
-			$reflectionObj = new \ReflectionObject($obj);
-			$reflectionProperties = $reflectionObj->getProperties();
-			foreach ($reflectionProperties as $property) {
-				$serializeAnnotation = $this->reader->getPropertyAnnotation($property, Serialize::class);
-				if ($serializeAnnotation == null) continue;
-				$name = $property->getName();
-				if ($serializeAnnotation->getName() != null)
-					$name = $serializeAnnotation->getName();
-				$className = $serializeAnnotation->getClassName();
-				if ($className != null) {
-					if (in_array($className, self::$specialTypes))
-						$result[$name] = $this->buildArrayFromSpecialType($property->getValue($obj), $className);
-					else
-						$result[$name] = $this->buildArray($property->getValue($obj));
-				} else {
-					$result[$name] = $property->getValue($obj);
+		if($obj != null) {
+			if (is_array($obj) || $obj instanceof Countable) {
+				foreach ($obj as $key => $value) {
+					$result[$key] = $this->buildArray($value);
+				}
+			} else if (is_string($obj) || is_bool($obj) || is_integer($obj) || is_float($obj) || is_double($obj)) {
+				$result = $obj;
+			} else if (is_array($obj)) {
+				$result[] = $this->buildArray($obj);
+			} else {
+				$reflectionObj = new \ReflectionObject($obj);
+				$reflectionProperties = $reflectionObj->getProperties();
+				foreach ($reflectionProperties as $property) {
+					$serializeAnnotation = $this->reader->getPropertyAnnotation($property, Serialize::class);
+					if ($serializeAnnotation == null) continue;
+					$name = $property->getName();
+					if ($serializeAnnotation->getName() != null)
+						$name = $serializeAnnotation->getName();
+					$className = $serializeAnnotation->getClassName();
+					if ($className != null) {
+						if (in_array($className, self::$specialTypes))
+							$result[$name] = $this->buildArrayFromSpecialType($property->getValue($obj), $className);
+						else
+							$result[$name] = $this->buildArray($property->getValue($obj));
+					} else {
+						$result[$name] = $property->getValue($obj);
+					}
 				}
 			}
 		}
